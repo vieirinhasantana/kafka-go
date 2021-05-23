13f31e54ddfb773299f8bd34c6f3817ef8205b60
Daniel V Santana <vieirinhasantana@gmail.com>
2021-05-23 10:36:40 -0300

    Change docker compose

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vieirinhasantana/kafka-go
 
 go 1.16
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.7.0
+)
---
+++ pkg/kafka/client.go
@@ -0,0 +1,42 @@
+package kafka
+
+import (
+	"fmt"
+	"log"
+
+	ckafka "gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
+
+type KafkaProcessor struct {
+	ConsumerTopics   []string
+	BootstrapServers string
+	ConsumerGroup    string
+}
+
+func NewKafkaProcessor() *KafkaProcessor {
+	return &KafkaProcessor{}
+}
+
+func (k *KafkaProcessor) Consume() {
+	c, err := ckafka.NewConsumer(&ckafka.ConfigMap{
+		"bootstrap.servers": k.BootstrapServers,
+		"group.id":          k.ConsumerGroup,
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics(k.ConsumerTopics, nil)
+	log.Printf("Kafka consumer has been started on host %s", k.BootstrapServers)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else {
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			c.Close()
+		}
+	}
+}
